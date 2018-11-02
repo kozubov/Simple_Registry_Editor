e275ea4e97aeada249182d4c5a0fe573cd122fda
Dmitry Kozubov <37287662+kozubov@users.noreply.github.com>
2018-11-02 12:56:52 +0200

    add

---
+++ SP_4/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SP_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_AutoRun = new System.Windows.Forms.ListBox();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.button_Remove = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox_AutoRun
+            // 
+            this.listBox_AutoRun.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox_AutoRun.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox_AutoRun.FormattingEnabled = true;
+            this.listBox_AutoRun.ItemHeight = 19;
+            this.listBox_AutoRun.Location = new System.Drawing.Point(11, 35);
+            this.listBox_AutoRun.Name = "listBox_AutoRun";
+            this.listBox_AutoRun.Size = new System.Drawing.Size(168, 308);
+            this.listBox_AutoRun.TabIndex = 0;
+            // 
+            // button_Add
+            // 
+            this.button_Add.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button_Add.Location = new System.Drawing.Point(200, 211);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(170, 55);
+            this.button_Add.TabIndex = 2;
+            this.button_Add.Text = "Add Prog to AutoRun";
+            this.button_Add.UseVisualStyleBackColor = true;
+            // 
+            // button_Remove
+            // 
+            this.button_Remove.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button_Remove.Location = new System.Drawing.Point(200, 281);
+            this.button_Remove.Name = "button_Remove";
+            this.button_Remove.Size = new System.Drawing.Size(170, 55);
+            this.button_Remove.TabIndex = 3;
+            this.button_Remove.Text = "Remove Prog from AutoRun";
+            this.button_Remove.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Programms in AutoRun";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 362);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_Remove);
+            this.Controls.Add(this.button_Add);
+            this.Controls.Add(this.listBox_AutoRun);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoRun Registry Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_AutoRun;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.Button button_Remove;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SP_4/Form1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button_Remove.Enabled = false;
+            LoadRegistry();
+            listBox_AutoRun.SelectedIndexChanged += ListBox_AutoRun_SelectedIndexChanged;
+            button_Add.Click += Button_Add_Click;
+            button_Remove.Click += Button_Remove_Click;
+        }
+
+        private void Button_Remove_Click(object sender, EventArgs e)
+        {
+            RegistryKey curUser = Registry.CurrentUser;
+            RegistryKey key = curUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            key.DeleteValue(listBox_AutoRun.SelectedItem.ToString());
+            key.Close();
+            curUser.Close();
+            listBox_AutoRun.Items.Clear();
+            LoadRegistry();
+        }
+
+        private void Button_Add_Click(object sender, EventArgs e)
+        {
+            DialogResult res = openFileDialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                AddProgramm();
+            }
+        }
+
+        private void AddProgramm()
+        {
+            RegistryKey curUser = Registry.CurrentUser;
+            RegistryKey key = curUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            string str = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+            key.SetValue(str, openFileDialog.FileName);
+            key.Close();
+            curUser.Close();
+            listBox_AutoRun.Items.Clear();
+            LoadRegistry();
+
+        }
+
+        private void ListBox_AutoRun_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button_Remove.Enabled = (listBox_AutoRun.SelectedItems.Count > 0);
+        }
+
+        private void LoadRegistry()
+        {
+            RegistryKey curUser = Registry.CurrentUser;
+            RegistryKey key = curUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+            foreach (var item in key.GetValueNames())
+            {
+                listBox_AutoRun.Items.Add(item);
+            }
+
+        }
+    }
+}
